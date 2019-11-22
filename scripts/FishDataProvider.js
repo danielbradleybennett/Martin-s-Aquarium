6ec3b6de055bd702d2c06a17ea4c94b90c33767c
DanielBradleyBennett <danielbradleybennett@gmail.com>
2019-11-22 10:54:21 -0600

    save martins

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,56 @@
+const fishCollection = [
+  {
+      name: "Alexis", 
+      location: "Amazon",
+      length: "10ft",
+      species: "snakefish",
+      food: ["fish", "flakes"],
+      img: "./styles/pictures/openMaw.jpg"
+  },
+
+
+
+  {
+      name: "Grandmus",
+      location: "pond",
+      length: "3inch",
+      species: "souleater",
+      food: "souls",
+      img: "fish.jpg",
+  },
+
+
+
+  {
+      name: "Loch",
+      location: "Loch Lomand",
+      length: "20ft",
+      species: "no flipping clue",
+      food: "picky eater",
+      img: "fish.jpg",
+  },
+
+
+
+  {
+      name: "Dandelion",
+      location: "Novir",
+      length: "5ft",
+      species: "goatfish",
+      img: "fish.jpg",
+  },
+
+
+
+
+  {
+      name: "Muse",
+      location: "Water Deep",
+      length: "5ft",
+      species: "demon fish",
+      food: "everything",
+  }
+]
+
+export const useFish = () => {
+  return fishCollection }
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,15 @@
 import initializeDetailButtonEvents from './dialog.js'
 import borderColorChange from './TipsBorder.js'
+import { useFish } from './FishDataProvider.js'
 
 
 initializeDetailButtonEvents()
 borderColorChange()
+const allOFTheFish = useFish()
+
+console.log("all of the fish:", allOFTheFish)
+
+for (const fish of allOFTheFish) {
+  console.log("individual fish object:", fish)
+
+}
